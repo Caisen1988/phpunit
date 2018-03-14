eced335f9d0bcd0c091499cc09f2da65c42e6b6a
william_ning <15237181025@163.com>
2018-03-14 14:30:29 +0800

    william push 代码 测试能否成功1

--- tests/CalculateServiceTest.php
+++ tests/CalculateServiceTest.php
@@ -6,14 +6,13 @@ use PHPUnit\Framework\TestCase;
 use Src;
 
 
-class CalculateServiceTest extends TestCase
-{
+class CalculateServiceTest extends TestCase {
     public function testAbs()
     {
 
         $ourservice = new Src\CalculateService();
 
-        $this->assertEquals(4, $ourservice->abs(-4));
+        $this->assertEquals(5, $ourservice->abs(-4));
 
     }
 }
