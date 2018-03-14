c53b46fa79574aa5bcbf59578e308b6778b6041f
samon_shi <shine_letsgood@163.com>
2018-03-14 15:00:29 +0800

    samon test

--- tests/CalculateServiceTest.php
+++ tests/CalculateServiceTest.php
@@ -10,6 +10,7 @@ class CalculateServiceTest extends TestCase
     public function testAbs()
     {
         echo 11222;
+        echo "samon";
         $ourservice = new Src\CalculateService();
 
         $this->assertEquals($ourservice->arr(), $ourservice->abs(233));
